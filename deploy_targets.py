d6aeb81b77adaeeebbb27ebb22e0c30c0ffe8c01
Andressa Sivolella <andressa@twistsystems.com>
2013-05-26 22:32:38 -0300

    Changing server (from harrison to sinatra) and cleading *.pyc

--- deploy_targets.py
+++ deploy_targets.py
@@ -3,6 +3,6 @@ import deploy
 class stage(deploy.SimpleTarget):
     GIT_REPOSITORY = 'git@github.com:andressa/projeto_integrado.git'
     GIT_BRANCH = 'stage'
-    SERVER = 'cc_mining@harrison.twistsystems.com'
+    SERVER = 'cc_mining@sinatra.twistsystems.com'
     DJANGO_DEPLOY_ENV = 'stage'
 
