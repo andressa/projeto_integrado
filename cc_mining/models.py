c700e493773646ab190b7cfd259cc2904dd72181
Andressa Sivolella <andressa@twistsystems.com>
2013-05-26 22:20:25 -0300

    Creating ClosedCaption class on cc_mining.models

--- cc_mining/models.py
+++ cc_mining/models.py
@@ -1,3 +1,12 @@
+"""
+cc_mining app models
+"""
+
 from django.db import models
 
-# Create your models here.
+class ClosedCaption(models.Model):
+    """
+    Class to describe acquired closed caption, line by line
+    """
+    timestamp = models.DateField(auto_now_add=True)
+    closed_caption = models.CharField(max_length=256)
